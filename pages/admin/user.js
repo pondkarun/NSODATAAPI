5a4239bac4417f45b63252073f45232c93fcea13
srw-lastseason <srw_lastseason@hotmail.com>
2021-06-19 22:52:02 +0700

    Admin management import layout

--- pages/admin/user.js
+++ pages/admin/user.js
@@ -1,7 +1,8 @@
 import styles from '../../styles/Home.module.scss';
 import Button from '@material-ui/core/Button';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector} from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,7 +11,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Layout from '../../components/Layout';
+import Layout from '../../components/Layouts';
 import CheckIcon from '@material-ui/icons/Check';
 import { green } from '@material-ui/core/colors';
 
