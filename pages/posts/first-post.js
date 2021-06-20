863f7e0a4925775ed3c9e9080b189b3ae35b0e20
srw-lastseason <srw_lastseason@hotmail.com>
2021-06-20 16:16:24 +0700

    fixed build bug

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,20 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../../components/Layout';
+// import Layout from '../../components/Layout';
 
 export default function FirstPost(){
-    // return <h1>First Post</h1>
-    return (
-        <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
-    )
+    return <h1>First Post</h1>
+    // return (
+    //     <Layout>
+    //   <Head>
+    //     <title>First Post</title>
+    //   </Head>
+    //   <h1>First Post</h1>
+    //   <h2>
+    //     <Link href="/">
+    //       <a>Back to home</a>
+    //     </Link>
+    //   </h2>
+    // </Layout>
+    // )
 }
