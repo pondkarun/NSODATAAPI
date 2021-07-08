3f8519388ddfb775828590159d79f1f1302c6488
srw-lastseason <srw_lastseason@hotmail.com>
2021-07-09 06:29:51 +0700

    Update page popular

--- pages/PopularDataset.js
+++ pages/PopularDataset.js
@@ -1,11 +1,15 @@
 import Layouts from '../components/Layouts';
+import Head from 'next/head';
 
 export default function AboutUS() {
     return (
-        <Layouts disableheader>
-            <h1 style={{ textAlign: 'center' }}>
-                Popular Data
-            </h1>
+        <Layouts>
+            <Head>
+                <title>ข้อมูลยอดนิยม</title>
+            </Head>
+            <h3 style={{fontSize: 30}}>
+                ข้อมูลยอดนิยม 0 รายการ
+            </h3>
         </Layouts>
         
     )
