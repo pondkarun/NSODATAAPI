9345bab14fdc6c151ae5ddbca63df84a5d68f7e5
Thunwa007 <vathunwa@gmail.com>
2021-07-11 23:58:04 +0700

    del jquery

--- pages/_document.js
+++ pages/_document.js
@@ -15,10 +15,6 @@ class MyDocument extends Document {
           <NextScript>
 
           </NextScript>
-          <script
-              src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
-              integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
-              crossorigin="anonymous"></script>
         </body>
       </Html>
     )
