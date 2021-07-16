84f26efb6e852cbf5395f03a30978ab46c85df6b
Thunwa007 <vathunwa@gmail.com>
2021-07-17 00:40:14 +0700

    fixconfig

--- next.config.js
+++ next.config.js
@@ -1,37 +1,23 @@
-// module.exports = {
-//   // productionBrowserSourceMaps: true,
-//   exportPathMap: async function (
-//     defaultPathMap,
-//     { dev, dir, outDir, distDir, buildId }
-//   ) {
-//     return {
-//       '/': { page: '/' },
-//       '/AboutUS': { page: '/AboutUS' },
-//       '/Admin/Report/Dashboard': { page: '/Admin/Report/Dashboard' },
-//       '/Admin/Permission': { page: '/Admin/Permission' },
-//       '/Admin/SystemFeature': { page: '/Admin/SystemFeature' },
-//       '/Admin/UserGroup': { page: '/Admin/UserGroup' },
-//       '/Admin/Users': { page: '/Admin/Users' },
-//       '/dataset': { page: '/dataset' },
-//       '/login': { page: '/login' },
-//       '/login/Admin': { page: '/login/Admin' },
-//       '/MyDatasetList': { page: '/MyDatasetList' },
-//       '/PopularDataset': { page: '/PopularDataset' },
-//     }
-//   },
-//   images: {
-//     loader: "imgix",
-//     path: "https://noop/",
-//   },
-//   trailingSlash: true,
-// }
 module.exports = {
   // productionBrowserSourceMaps: true,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    return defaultPathMap
+    return {
+      '/': { page: '/' },
+      '/AboutUS': { page: '/AboutUS' },
+      '/Admin/Report/Dashboard': { page: '/Admin/Report/Dashboard' },
+      '/Admin/Permission': { page: '/Admin/Permission' },
+      '/Admin/SystemFeature': { page: '/Admin/SystemFeature' },
+      '/Admin/UserGroup': { page: '/Admin/UserGroup' },
+      '/Admin/Users': { page: '/Admin/Users' },
+      '/dataset': { page: '/dataset' },
+      '/login': { page: '/login' },
+      '/login/Admin': { page: '/login/Admin' },
+      '/MyDatasetList': { page: '/MyDatasetList' },
+      '/PopularDataset': { page: '/PopularDataset' },
+    }
   },
   images: {
     loader: "imgix",
@@ -39,3 +25,17 @@ module.exports = {
   },
   trailingSlash: true,
 }
+// module.exports = {
+//   // productionBrowserSourceMaps: true,
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return defaultPathMap
+//   },
+//   images: {
+//     loader: "imgix",
+//     path: "https://noop/",
+//   },
+//   trailingSlash: true,
+// }
---
+++ pages/dataset/contens.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contens = () => {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Contens
